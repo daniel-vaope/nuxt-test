19f4805a673f07dd6e0a1985080a438b8f5b874c
Daniel Villafranqui <dvillafranqui@vaope.com>
2026-02-25 10:57:45 -0500

    send param to route with input

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
   <h1>Hola mundo</h1>
+  <input v-model="postID" @keydown.enter="openPost"/>
+  <button @click="openPost">Go to post {{ postID }}</button>
 </template>
+
+<script setup>
+
+const postID = ref("");
+
+function openPost() {
+    navigateTo('/blog/' + postID.value)
+}
+
+</script>
