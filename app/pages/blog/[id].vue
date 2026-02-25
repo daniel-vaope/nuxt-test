34fe9a0d243cbda8de7509faf6e3f85d7259a08d
Daniel Villafranqui <dvillafranqui@vaope.com>
2026-02-25 10:43:31 -0500

    send params in route with methods

--- app/pages/blog/[id].vue
+++ app/pages/blog/[id].vue
@@ -1,5 +1,20 @@
 <!-- Ruta dinámica dentro de la ruta /blog/{id} -->
 <template>
-    <!-- Getting ID from route params -->
-    <h1>Post {{ $route.params.id }}</h1>
+  <!-- Getting ID from route params -->
+  <h1>Post {{ $route.params.id }}</h1>
+  <button @click="sendParams(123)">
+    Enviar parametros
+  </button>
 </template>
+
+<script>
+export default {
+  methods: {
+    sendParams(id) {
+      // Additional logic
+      console.log(id);
+      this.$router.push("/blog/" + id);
+    },
+  },
+};
+</script>
