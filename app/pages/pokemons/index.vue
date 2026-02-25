b05613c51000c7e557b19675b1a46a14c41262d2
Daniel Villafranqui <dvillafranqui@vaope.com>
2026-02-25 15:32:57 -0500

    consuming api rest with axios

---
+++ app/pages/pokemons/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Hola mundo</h1>
+
+  <table>
+    <thead>
+      <tr>
+        <th>Nombre</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in pokemons" :key="index">
+        <td>{{ item.name }}</td>
+        <td>
+          <NuxtLink to="/">Ver más</NuxtLink>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script setup>
+import axios from "axios";
+
+const pokemons = ref([]);
+
+onMounted(() => {
+  axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+    pokemons.value = res.data.results;
+  });
+});
+</script>
