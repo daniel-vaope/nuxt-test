1b006879160c77217aeb1c46b3dd706dd5ca4c20
Daniel Villafranqui <dvillafranqui@vaope.com>
2026-02-25 10:48:28 -0500

    update to vue composition API

--- app/pages/blog/[id].vue
+++ app/pages/blog/[id].vue
@@ -2,19 +2,13 @@
 <template>
   <!-- Getting ID from route params -->
   <h1>Post {{ $route.params.id }}</h1>
-  <button @click="sendParams(123)">
-    Enviar parametros
-  </button>
+  <button @click="sendParams(123)">Enviar parametros</button>
 </template>
 
-<script>
-export default {
-  methods: {
-    sendParams(id) {
-      // Additional logic
-      console.log(id);
-      this.$router.push("/blog/" + id);
-    },
-  },
-};
+<script setup>
+function sendParams(id) {
+  // Additional logic
+  console.log(id);
+  // this.$router.push("/blog/" + id);
+}
 </script>
